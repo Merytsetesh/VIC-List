99d0b8617b2566f9f5d22a13d5fab76ff63f0723
Simon <merytsetesh@gmail.com>
2019-08-04 23:18:53 -0400

    Major update. All glyph values are now returned correctly, including PETSCII block characters with both SHIFT and CBM, in all four character modes.

--- Glyphs.py
+++ Glyphs.py
@@ -9,14 +9,22 @@
 # Copyright (C) 2019 Simon Ellis.
 
 
-REVERSE   = 0
 REV_BYTE  = 128
-LOWERCASE = 0
 LOW_BYTE  = 256
 
 
+# Indicates a non-printable or invalid glyph.
+
+NOGLYPH   =  -1
+
+
+# This dictionary contains mappings for special VIC control characters (upper block) and shifted
+# non-alphabetic characters (second block). Control characters map to symbols in the reverse block.
+#
+# As the return character isn't displayed,
+
 Special =     { "{WHI}"          : 133,
-                "{RETURN}"       :  -1,
+                "{RETURN}"       : NOGLYPH,
                 "{CRSR-DOWN}"    : 145,
                 "{RVS-ON}"       : 146,
                 "{HOME}"         : 147,
@@ -33,7 +41,7 @@ Special =     { "{WHI}"          : 133,
                 "{F4}"           : 202,
                 "{F6}"           : 203,
                 "{F8}"           : 204,
-                "{SHF-RTRN}"     :  -1,
+                "{SHF-RTRN}"     : NOGLYPH,
                 "{BLK}"          : 207,
                 "{CRSR-UP}"      : 209,
                 "{RVS-OFF}"      : 210,
@@ -54,13 +62,30 @@ Special =     { "{WHI}"          : 133,
                 "{SHF-@}"        : 122,
                 "{CBM-*}"        :  95,
                 "{SHF-*}"        :  64,
-                "{SHF-UP-ARROW}" :  94
+                "{SHF-UP-ARROW}" :  94,
+                "{CBM-UP-ARROW}" :  94
               }
 
 
-def GetGlyph(sym):
+# The block character set reached using the Commodore key is a pain. The ordering of the characters
+# in the glyphset follows no logic I can see. No number keys map to CBM symbols, and only some of
+# the non-alpha ones. Shift-Up-Arrow and CBM-Up-Arrow map to the same symbol.
+#
+# This string maps from an alpha character to a CBM glyph in the range 96-127. Spaces are caused
+# by symbols in the dictionary above mapping to that codepoint.
+
+CBMLETTERS = " KIT G M  NQDZSPAERWHJLYUO FCXVB"
+
+
+# Some ASCII symbols do not have block graphics. These require special handling.
+
+NOGRAPHICS = " !\"#$%&'()*+,-./0123456789:;<=>?"
+
+
+def GetGlyph(sym, reverse=False, lower=False):
     sym = sym.upper()
     offset = 0
+    glyph = NOGLYPH
 
     if sym in Special.keys():
         glyph = Special[sym]
@@ -74,30 +99,41 @@ def GetGlyph(sym):
         else:
             c = sym
 
-        if "CBM" not in sym:
-            glyph = -1
+        if '[' in c or ']' in c or c in NOGRAPHICS:
+            if offset != 0:
+                glyph = NOGLYPH
+            else:
+                if "[" in c:
+                    glyph = 27
+                elif "]" in c:
+                    glyph = 29
+                else:
+                    glyph = 32 + NOGRAPHICS.index(c)
+
+        elif "UKP" in sym or "ARROW" in sym:
             if "UKP" in sym:
                 glyph = 28
             elif "UP-ARROW" in sym:
-                glyph = 30
+                if offset != 96:
+                    glyph = 30
             elif "LEFT-ARROW" in sym:
-                glyph = 31
-            elif "[" in sym:
-                glyph = 27
-            elif "]" in sym:
-                glyph = 29
-            elif 64 <= ord(c) <= 90:
-                glyph = ord(c) - 64
-            elif c in " !\"#$%&'()*+,-./0123456789:;<=>?":
-                glyph = 32 + " !\"#$%&'()*+,-./0123456789:;<=>?".index(c)
-        else:
-            glyph = " KIT G M  NQDZSPAERWHJYUO FCXVB".index(c)
+                if offset == 0:
+                    glyph = 31
 
-    if glyph > -1:
-        if glyph < 129 and REVERSE == 1:
+        elif offset == 96:
+            if len(sym) > 7 or c not in CBMLETTERS:
+                glyph = NOGLYPH
+            else:
+                glyph = CBMLETTERS.index(c)
+
+        elif 64 <= ord(c) <= 90:
+            glyph = ord(c) - 64
+
+    if glyph != NOGLYPH:
+        if glyph < 129 and reverse:
             glyph += REV_BYTE
-
-        if LOWERCASE == 1:
+        if lower:
             glyph += LOW_BYTE
+        return glyph + offset
 
-    return glyph + offset
+    return NOGLYPH
